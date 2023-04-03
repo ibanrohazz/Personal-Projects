9fe4e473dfd7bd19c35829ff51088c38d15f5af2
ibanrohazz <itr5720@g.rit.edu>
2023-04-03 17:42:56 -0400

    4/3/23 42 'trapping water'

--- L33tCode/37-SodokuSolverBetter.java
+++ L33tCode/37-SodokuSolverBetter.java
@@ -5,7 +5,7 @@ class Solution {
 
     private boolean[][] rows = new boolean[9][10];
     private boolean[][] cols = new boolean[9][10];
-    private boolean[][] subboxes = new boolean[9][10];
+    private boolean[][] sub = new boolean[9][10];
 
     public void solveSudoku( char[][] board) {
         int row = board.length, col = board[0].length;
@@ -17,7 +17,7 @@ class Solution {
                     int idx = c -'0';
                     rows[i][idx] = true;
                     cols[j][idx] = true;
-                    subboxes[mapping(i, j)][idx] = true;
+                    sub[mapping(i, j)][idx] = true;
                 } else if( c == '.' ) {
                     empty.add(i * 9 + j);
                 }
@@ -35,17 +35,17 @@ class Solution {
         int pos = empty.get(idx);
         int row = pos / 9, col = pos % 9;
         for( int i = 1; i <= 9; i++ ) {
-            if( !rows[row][i] && !cols[col][i] && !subboxes[mapping(row, col)][i] ) {
+            if( !rows[row][i] && !cols[col][i] && !sub[mapping(row, col)][i] ) {
                 rows[row][i] = true;
                 cols[col][i] = true;
-                subboxes[mapping(row, col)][i] = true;
+                sub[mapping(row, col)][i] = true;
                 board[row][col] = (char) (i + '0');
                 if( dfs(board, empty, idx + 1) ) {
                     return true;
                 }
                 rows[row][i] = false;
                 cols[col][i] = false;
-                subboxes[mapping(row, col)][i] = false;
+                sub[mapping(row, col)][i] = false;
                 board[row][col] = '.';
             }
         }
---
+++ L33tCode/42-TrappingRainWater.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int trap(int[] height) {
+        // given n non-negative integers repersenting an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+        // 1. find the highest bar
+        // 2. from left to right, find the highest bar on the left of the current bar
+        // 3. from right to left, find the highest bar on the right of the current bar
+        // 4. the water trapped by the current bar is the min of the two highest bars on the left and right minus the height of the current bar
+        // 5. sum up the water trapped by each bar
+        int n = height.length;
+        if( n == 0 ) {
+            return 0;
+        }
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = height[0];
+        right[n - 1] = height[n - 1];
+        for( int i = 1; i < n; i++ ) {
+            left[i] = Math.max(left[i - 1], height[i]);
+        }
+        for( int i = n - 2; i >= 0; i-- ) {
+            right[i] = Math.max(right[i + 1], height[i]);
+        }
+        int sum = 0;
+        for( int i = 0; i < n; i++ ) {
+            sum += Math.min(left[i], right[i]) - height[i];
+        }
+        return sum;
+    }
+}
