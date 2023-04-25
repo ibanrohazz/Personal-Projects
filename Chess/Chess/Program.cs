bca05dd642f15f5634a44d307552484c103d5f7a
ibanrohazz <itr5720@g.rit.edu>
2023-04-25 09:26:24 -0400

    4/25/23

--- Chess/Chess/Program.cs
+++ Chess/Chess/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
 using System;
 
 namespace HelloWorld
@@ -11,3 +14,4 @@ namespace HelloWorld
         }
     }
 }
+*/
